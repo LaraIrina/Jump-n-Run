afa996d2ebda0666705ea7f2b9ab7f4211247a77
LaraIrina <89276310+LaraIrina@users.noreply.github.com>
2023-11-05 20:03:05 +0100

    Update index.js
    Added more functions for the character

--- index.js
+++ index.js
@@ -20,3 +20,48 @@ $('.cloud').on("click",function() {
     $('.cloud').css('background', "#ECEFF1"); //Cloud Color back to original value
   }, 1000); // 1000 ms (1 second)
 });
+
+
+// Detecting Keyboard Press for movement of character
+document.addEventListener("keydown", function (event) {
+  // Right Arrow click to move forward
+  if (event.keyCode == 39) {
+    forward();
+  }
+   // Left Arrow click to move backwards
+  if (event.keyCode == 37) {
+    backwards();
+  }
+  // Up Arrow to jump
+  if (event.keyCode == 38) {
+    jump();
+    setTimeout(function() {
+      $('.character').css('bottom','-100px'); //Bottom back to original value
+    }, 200); // 200 ms (1 second)
+  }
+});
+
+function forward() {
+  // get current position in px
+  const currentPosition = parseInt($('.character').css('left'));
+  // change current position: add 10px
+  const newPosition = currentPosition + 10;
+  // move character to new position
+  $('.character').css('left', newPosition + 'px');
+}
+function backwards() {
+  // get current position in px
+  const currentPosition = parseInt($('.character').css('left'));
+  // change current position: add 10px
+  const newPosition = currentPosition - 10;
+  // move character to new position
+  $('.character').css('left', newPosition + 'px');
+}
+function jump() {
+  // get current position in px
+  const currentPosition = parseInt($('.character').css('bottom'));
+  // change current position: add 20px
+  const newPosition = currentPosition + 100;
+  // move character to new position
+  $('.character').css('bottom', newPosition + 'px');
+}
